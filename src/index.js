5d649ee4ca495ed529cd683f4aa3d2a5d93f24db
anangkf_ <gonanggoneng@gmail.com>
2022-10-27 14:32:27 +0700

    try/mui-joy: try implement custom style using sx props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CssVarsProvider>
+      {/* must be used under CssVarsProvider */}
+      <CssBaseline />
+
+      {/* The rest of your application */}
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>
 );
