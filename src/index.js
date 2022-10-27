3103b9dd6c6ace3c0e7735798bfaf3d46fab64e1
anangkf_ <gonanggoneng@gmail.com>
2022-10-27 15:46:56 +0700

    setup color variables

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
-import './index.css';
 import SetupRouter from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/themes/index.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/system";
+
+export const theme = createTheme({
+  vars:{
+    yellow: '#FF9F1C',
+    dark: '#011627',
+    secondary: '#475356',
+    light: '#FDFFFC',
+    softGray: '#EDE1E1',
+    blue: '#2E86AB',
+    softBlue: '#2EC4B6',
+    green: '#3DC926',
+    red: '#F24236',
+  }
+})
