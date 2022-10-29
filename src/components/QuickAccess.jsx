5d79995e69e81ba8274e8584bd45a876d9288442
anangkf_ <gonanggoneng@gmail.com>
2022-10-29 13:41:34 +0700

    feat/dashboard: added quick access component

--- src/components/QuickAccess.jsx
+++ src/components/QuickAccess.jsx
@@ -1,27 +1,72 @@
-import { Box, Button, Typography } from '@mui/joy'
+import { AddCircle, KeyboardArrowDown } from '@mui/icons-material'
+import { Box, Button, Input, Option, Select, selectClasses, Typography } from '@mui/joy'
 import React from 'react'
+import { theme } from '../themes'
 import BoxWrapper from './BoxWrapper'
 
 const QuickAccess = () => {
+  const celenganList = ['Beli motor', 'Buat nikah', 'Beli PC', 'Naik haji']
   return (
     <BoxWrapper>
       <Typography
-          fontSize= {'xl3'}
+        fontSize= {'xl3'}
+        sx={{
+          fontWeight: 600
+        }}
+      >
+        Akses cepat
+      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column'
+        }}
+      >
+        <Box
           sx={{
-            fontWeight: 600
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            gap: 2,
+            my: 2
           }}
         >
-          Akses cepat
-          </Typography>
-          {/* <Typography fontSize={'lg'}>Semangat menabung yaa</Typography> */}
-          <Box
+          <Select
+            placeholder="Pilih celengan…"
+            indicator={<KeyboardArrowDown />}
             sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              gap: 2
+              width: '48%',
+              [`& .${selectClasses.indicator}`]: {
+                transition: '0.2s',
+                [`&.${selectClasses.expanded}`]: {
+                  transform: 'rotate(-180deg)',
+                },
+              },
             }}
           >
-          </Box>
+            {celenganList.map((item, idx) =>{
+              return(
+                <Option key={idx} value={item}>{item}</Option>
+              )
+            })}
+          </Select>
+          <Input type='number' sx={{width: '48%'}} placeholder="Masukkan nominal…" />
+        </Box>
+        <Button 
+          startDecorator={<AddCircle />}
+          sx={{
+            backgroundColor: theme.vars.dark,
+            width: '120px',
+            '&:hover':{
+              backgroundColor: theme.vars.dark,
+              opacity: 0.9
+            }
+          }}
+        >
+          celengin
+        </Button>
+      </Box>
     </BoxWrapper>
   )
 }
